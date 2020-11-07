d069012c7e4dbf705efb4b3a743af94c78ce1c60
SumitomShome <sumitomshome@gmail.com>
2020-11-08 00:41:10 +0530

    [Sumitom] Refactor: Managing conflicts

--- HotelReservationProgram/HotelReservationClass.cs
+++ HotelReservationProgram/HotelReservationClass.cs
@@ -5,20 +5,7 @@ namespace HotelReservationProgram
 {
     public class HotelReservationClass
     {
-<<<<<<< HEAD
-        private string hotelName;
-        private int hotelRate;
-        private int totalRate;
-        private int weekDaysRate;
-        private int weekendRate;
-        private int rating;
-        private int numberOfWeekDays = 0;
-        private int numberOfWeekends = 0;
-        private Dictionary<string, HotelDetails> hotels = new Dictionary<string, HotelDetails>();
-        public string AddHotelName(string hotelName, int hotelRate)
-=======
         public HotelType FindCheapestHotel(string startDate, string endDate, CustomerType type)
->>>>>>> UC2-FindCheapestHotel
         {
             HotelDetails LakeWood = new HotelDetails(HotelType.LAKEWOOD, type);
             HotelDetails BridgeWood = new HotelDetails(HotelType.BRIDGEWOOD, type);
@@ -42,35 +29,6 @@ namespace HotelReservationProgram
                 return HotelType.BRIDGEWOOD;
             return HotelType.RIDGEWOOD;
         }
-<<<<<<< HEAD
-        public int GetCheapestHotel(string nameOfHotel, int weekDaysRateOfHotel, int weekendRateOfHotel)
-        {
-            HotelDetails hotelDetails = new HotelDetails();
-            hotelDetails.HotelName = nameOfHotel;
-            hotelDetails.WeekDayHotelRates = weekDaysRateOfHotel;
-            hotelDetails.WeekendHotelRates = weekendRateOfHotel;
-            int noOfDays;
-            Console.WriteLine("Enter the date of check in");
-            DateTime day1 = Convert.ToDateTime(Console.ReadLine());
-            string date1 = day1.DayOfWeek.ToString();
-            Console.WriteLine("Enter the date of checkout");
-            DateTime day2 = Convert.ToDateTime(Console.ReadLine());
-            string date2 = day2.DayOfWeek.ToString();
-            noOfDays = (day2 - day1).Days;
-            foreach (var lodge in hotels)
-            {
-                if (date1 == "Saturday" || date1 == "Sunday" || date2 == "Saturday" || date2 == "Sunday")
-                {
-                    totalRate = GetWeekDaysRate(nameOfHotel, weekendRateOfHotel, noOfDays);
-                }
-                else
-                {
-                    totalRate = GetWeekendRate(nameOfHotel, weekDaysRateOfHotel, noOfDays);
-                }
-                //totalRate = hotelRate * noOfDays;
-            }
-            return totalRate;
-=======
         public HotelType FindBestRatedHotel(string startDate, string endDate, CustomerType type)
         {
             HotelDetails RidgeWood = new HotelDetails(HotelType.RIDGEWOOD, type);
@@ -83,7 +41,6 @@ namespace HotelReservationProgram
                 return HotelType.BRIDGEWOOD;
             else
                 return HotelType.LAKEWOOD;
->>>>>>> UC2-FindCheapestHotel
         }
         public int GetWeekDaysRate(string nameOfTheHotel, int rateOfTheHotel, int numberOfDays)
         {
--- HotelReservationProgram/Program.cs
+++ HotelReservationProgram/Program.cs
@@ -6,28 +6,7 @@ namespace HotelReservationSystem
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Welcome to Hotel Registration Program");
-            HotelReservationClass hotelReservationClass = new HotelReservationClass();
-            //hotelReservationClass.AddHotelName("Paradise", 800);
-            //hotelReservationClass.AddHotelName("Joseph", 1000);
-            //hotelReservationClass.AddHotelName("Pranton", 600);
-            if(hotelReservationClass.GetCheapestHotel("Paradise", 800, 1000) < hotelReservationClass.GetCheapestHotel("Joseph", 1000, 1200) && hotelReservationClass.GetCheapestHotel("Paradise", 800, 1000) < hotelReservationClass.GetCheapestHotel("Pranton", 600, 800))
-            {
-                    Console.WriteLine("The name of the cheapest hotel is Paradise");
-            }
-            else if (hotelReservationClass.GetCheapestHotel("Joseph", 800, 1000) < hotelReservationClass.GetCheapestHotel("Pranton", 600, 800))
-            {
-                Console.WriteLine("The name of the cheapest hotel is Joseph");
-            }
-            else
-            {
-                Console.WriteLine("The name of the cheapest hotel is Pranton");
-            }
-            Console.ReadKey();
-=======
             Console.WriteLine("Welcome to the Hotel Reservation Program");
->>>>>>> UC2-FindCheapestHotel
         }
     }
 }
