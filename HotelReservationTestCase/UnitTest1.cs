f958e9fcfc7f4262a2d596f376eba52b6dfc8de0
SumitomShome <sumitomshome@gmail.com>
2020-11-08 19:39:27 +0530

    [Sumitom] Refactor: Method to find cheapest hotel based on weekdays and weekend

--- HotelReservationTestCase/UnitTest1.cs
+++ HotelReservationTestCase/UnitTest1.cs
@@ -16,13 +16,20 @@ namespace HotelReservationSystemTest
             double expectedRate = 110;
             Assert.AreEqual(expectedRate, hotelDetails.WEEKDAY_RATE);
         }
-
         [TestMethod]
-        public void FindTheCheapestHotel()
+        public void FindCheapestWeekendRate()
         {
             HotelReservationClass service = new HotelReservationClass();
-            HotelType hotel = service.FindCheapestHotel("2020-09-11", "2020-09-12", CustomerType.NORMAL);
-            HotelType expected = HotelType.RIDGEWOOD;
+            HotelType hotel = service.FindCheapestHotel("2020-09-12", "2020-09-13", CustomerType.NORMAL);
+            HotelType expected = HotelType.BRIDGEWOOD;
+            Assert.AreEqual(hotel, expected);
+        }
+        [TestMethod]
+        public void FindCheapestWeekdaysRate()
+        {
+            HotelReservationClass service = new HotelReservationClass();
+            HotelType hotel = service.FindCheapestHotel("2020-09-08", "2020-09-09", CustomerType.NORMAL);
+            HotelType expected = HotelType.LAKEWOOD;
             Assert.AreEqual(hotel, expected);
         }
     }
